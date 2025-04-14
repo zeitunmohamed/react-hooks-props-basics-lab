79bc95da18928a3d67ef04ce50a240ae0177d414
zeitunmohamed <zeitunmohamed@gmail.com>
2025-04-14 21:47:51 +0300

    complete lab

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <br />
+      <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
